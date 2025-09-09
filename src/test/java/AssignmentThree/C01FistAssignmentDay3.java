85d8239eb51f1c4570b0842a8a511cb236f2fac8
Misbah <mesbahghamdi386@gmail.com>
2025-09-10 00:03:41 +0300

    Ass3

---
+++ src/test/java/AssignmentThree/C01FistAssignmentDay3.java
@@ -0,0 +1,61 @@
+package AssignmentThree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class C01FistAssignmentDay3{
+
+    WebDriver driver;
+
+    // Navigate to: https://opensource-demo.orangehrmlive.com/
+
+        @Test
+        public void testNavigation(){
+
+            driver = new ChromeDriver();
+            driver.get("https://opensource-demo.orangehrmlive.com/");
+
+            // TODO: Locate username using absolute XPath
+            String absoluteUsernameXPath = "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]"; // Fill absolute path for Username
+            String absolutePasswordXPath = "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div"; // Fill absolute path for Password
+
+
+            // TODO: Locate username using relative XPath
+            String relativeUsernameXPath = "//input[@name='username']"; // Fill relative path
+            String relativePasswordXPath = "//input[@name='password']";
+            String relativeLoginXPath = "//button[@type='submit']";
+
+
+
+            // TODO: Locate password using both methods
+            // TODO: Locate login button using both methods
+            // TODO: Test both approaches work
+
+           WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+           WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='username']")));
+
+
+            WebElement usernameAbsolute = driver.findElement(By.xpath(absoluteUsernameXPath));
+            WebElement usernameRelative = driver.findElement(By.xpath(relativeUsernameXPath));
+
+            Assertions.assertTrue(usernameAbsolute.isDisplayed());
+            Assertions.assertTrue(usernameRelative.isDisplayed());
+
+            driver.quit();
+
+
+
+
+        }
+    }
+
+
---
+++ src/test/java/AssignmentThree/C02SecondAssingment.java
@@ -0,0 +1,65 @@
+package AssignmentThree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C02SecondAssingment {
+
+    // TODO: Navigate to test pages
+    WebDriver driver;
+
+
+    @Test
+    public void TestNavigation(){
+
+        driver = new ChromeDriver();
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+
+
+        // TODO: Click on "Locators - Find By Playground Test Page"
+        driver.findElement(By.linkText("Locators - Find By Playground Test Page")).click();
+
+        // TODO: Print current URL
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+
+        // TODO: Navigate back
+        driver.navigate().back();
+
+        // TODO: Print URL after going back
+        System.out.println( "URL after going back: " + driver.getCurrentUrl());
+
+
+        // TODO: Click on "WebDriver Example Page"
+        driver.findElement(By.linkText("WebDriver Example Page")).click();
+
+        // TODO: Print current URL
+        System.out.println("WebDriver page URL: " + driver.getCurrentUrl());
+
+        // TODO: Enter value 20 in number input
+        driver.findElement(By.id("numentry")).sendKeys("20");
+
+        // I try to enter value by this way but didn't work .
+
+//       WebElement numberInput = driver.findElement(By.id("number"));
+//       numberInput.sendKeys("20");
+//       numberInput.sendKeys(Keys.ENTER);
+
+        // TODO: Verify 'two, zero' message appears
+
+        WebElement message = driver.findElement(By.id("message"));
+        Assertions.assertTrue(message.getText().contains("two,zero"));
+
+        driver.quit();
+
+
+
+    }
+
+
+
+}
